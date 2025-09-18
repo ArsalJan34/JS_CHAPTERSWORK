c5c659b9a038d52c7a271c6bf8a667b5eb0d1d17
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-18 18:36:55 +0500

    Changes made

---
+++ Chapter5/Q3/script.js
@@ -0,0 +1,13 @@
+var a;
+    document.write("<h1>Q3:</h1>");
+    document.write("Value after variable declaration is: " + a + "<br>");
+    a = 5;
+    document.write("Initial value: " + a + "<br>");
+    a++;
+    document.write("Value after increment is: " + a + "<br>");
+    a = a + 7;
+    document.write("Value after addition is: " + a + "<br>");
+    a--;
+    document.write("Value after decrement is: " + a + "<br>");
+    var remainder = a % 3;
+    document.write("The remainder is: " + remainder + "<br>");
