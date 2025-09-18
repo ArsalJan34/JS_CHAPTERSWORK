e424d380a6dd44790159016b5913d608b5c204ff
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-18 17:39:56 +0500

    Q4

---
+++ Chapter2/Q4/script.js
@@ -0,0 +1,6 @@
+  var studentName = "Ali Ahmed";
+    var studentAge = "21 years old";
+    var studentCourse = "Certified Web Development";
+    alert(studentName);
+    alert(studentAge);
+    alert(studentCourse);
