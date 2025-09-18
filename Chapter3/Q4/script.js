6e6397520d991fca7fea67c3b63e544be3ba05bb
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-18 18:09:32 +0500

    Changes made

--- Chapter3/Q4/script.js
+++ Chapter3/Q4/script.js
@@ -1,9 +1,4 @@
   var visitorName = "John Doe";
-
-   
     var productTitle = "T-shirt";
-
-   
     var quantity = 5;
-
     document.write(visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store");
