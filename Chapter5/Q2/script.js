e7cdd579beec23eb9fc281c43ec66fd2efd8cf66
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-18 18:31:26 +0500

    Changes made

---
+++ Chapter5/Q2/script.js
@@ -0,0 +1,5 @@
+  document.write("<h1>Q2:</h1>");
+    document.write("Subtraction: " + (num1 - num2) + "<br>");
+    document.write("Multiplication: " + (num1 * num2) + "<br>");
+    document.write("Division: " + (num1 / num2) + "<br>");
+    document.write("Modulus: " + (num1 % num2) + "<br>");
