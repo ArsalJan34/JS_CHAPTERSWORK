83086b8a41f8da8fc48d356c553817968231101c
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-19 22:25:34 +0500

    Changes made

--- Chapter5/Q4/script.js
+++ Chapter5/Q4/script.js
@@ -1,3 +1,3 @@
  var ticketPrice = 600;
     var totalCost = ticketPrice * 5;
-    document.write("<h3>Q4: Total cost to buy 5 tickets is " + totalCost + " PKR</h3>");
+    document.write("<h1>Q4: Total cost to buy 5 tickets is " + totalCost + " PKR</h1>");
---
+++ Chapter5/Q6/script.js
@@ -0,0 +1,7 @@
+ var celsius = 25;
+    var fahrenheit = (celsius * 9/5) + 32;
+    document.write("<h1>Q6:</h1>");
+    document.write(celsius + "°C is " + fahrenheit + "°F<br>");
+    var fTemp = 70;
+    var cTemp = (fTemp - 32) * 5/9;
+    document.write(fTemp + "°F is " + cTemp + "°C<br>");
