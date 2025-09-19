b1cf67ebdf903ba8e3eb1b44d191258e6ed82a2c
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-19 23:06:34 +0500

    Changes made

---
+++ Chapter5/Q11/script.js
@@ -0,0 +1,5 @@
+ var currentYear = 2025, birthYear = 2004;
+    var age1 = currentYear - birthYear;
+    var age2 = age1 - 1;
+    document.write("<h3>Q11: Age Calculator</h3>");
+    document.write("They are either " + age1 + " or " + age2 + " years old<br>");
