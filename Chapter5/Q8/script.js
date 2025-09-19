eca91a56535932de76ed8d9e83c1be8d0f4ad546
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-19 22:29:08 +0500

    Changes made

---
+++ Chapter5/Q8/script.js
@@ -0,0 +1,6 @@
+ var totalMarks = 980, marksObtained = 804;
+    var percentage = (marksObtained / totalMarks) * 100;
+    document.write("<h2>Q8:</h2>");
+    document.write("Total Marks: " + totalMarks + "<br>");
+    document.write("Marks Obtained: " + marksObtained + "<br>");
+    document.write("Percentage: " + percentage + "%<br>");
