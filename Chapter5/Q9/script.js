b1874889a124183a4f3deb320427621bf67eba02
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-19 22:30:05 +0500

    Changes made

---
+++ Chapter5/Q9/script.js
@@ -0,0 +1,4 @@
+ var dollars = 10, riyals = 25;
+    var totalPKR = (dollars * 104.80) + (riyals * 28);
+    document.write("<h2>Q9: Currency in PKR</h2>");
+    document.write("Total Currency in PKR: " + totalPKR + "<br>");
