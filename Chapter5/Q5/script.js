0650f18c390781c25e105ca031ede11fd3557fe0
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-19 22:24:16 +0500

    Changes made

---
+++ Chapter5/Q5/script.js
@@ -0,0 +1,6 @@
+
+    var tableNum = 4;
+    document.write("<h1>Q5 : Multiplication table of "+ tableNum + "</h1>" );
+    for (var i=1; i<=10; i++){
+        document.write(tableNum+" x "+i+ " = "+(tableNum * i)+"<br>");
+    }
