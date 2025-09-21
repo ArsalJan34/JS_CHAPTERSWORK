10fbc9e790bc138f1c5367817f840edba3e9d60b
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-21 09:30:50 +0500

    Changes made

--- Chapter6/Q1/script.js
+++ Chapter6/Q1/script.js
@@ -1,6 +1,15 @@
 var a = 10;
 document.write("<h3>Result<h3>");
-document.write("<h4>The value of num : "+num+ "<br>" );
+document.write("<h4>The value of a : "+ a + "<br>" );
+++a;
+document.write("The value of ++a is : " + a +"<br>");
+document.write("Now the value of a is : "+ a +"<br>");
 a++;
-document.write("The value of a++ is : "+a+"<br>");
-document.write("Now the value of a is : "+a+"<br>")
+document.write("The value of a++ is :"+a+"<br>");
+document.write("Now the value of a is : "+a+"<br>");
+--a;
+document.write("The value of --a is : "+a+"<br>");
+document.write("Now the value of a is : "+a+"<br>");
+a--;
+document.write("The value of a-- is : "+a+"<br>");
+document.write("Now the value of a is : "+a+"<br>");
