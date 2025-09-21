3c79ee824388091f77ab356d08890bb3a302ae0c
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-21 09:23:32 +0500

    Changes made

---
+++ Chapter6/Q1/script.js
@@ -0,0 +1,6 @@
+var a = 10;
+document.write("<h3>Result<h3>");
+document.write("<h4>The value of num : "+num+ "<br>" );
+a++;
+document.write("The value of a++ is : "+a+"<br>");
+document.write("Now the value of a is : "+a+"<br>")
