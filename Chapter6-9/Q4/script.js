b5217155b6d32c7c9fff71b8719fb2c92943a44b
ArsalJan34 <arsaljaan532@gmail.com>
2025-09-21 12:24:30 +0500

    Changes made

--- Chapter6-9/Q4/script.js
+++
@@ -1,8 +0,0 @@
-
-  var num = prompt("Enter a number for multiplication table:", "5") || 5;
-
-  document.write("<h3>Multiplication Table of " + num + "</h3>");
-  for (var i = 1; i <= 10; i++) {
-    document.write(num + " x " + i + " = " + (num * i) + "<br>");
-  }
-
